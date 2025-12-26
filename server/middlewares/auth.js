247561f966a46827c483e6e9f2ae1897edc9bfa2
Manan Batra <mananbatra1946@gmail.com>
2025-12-26 14:45:20 +0530

    feat: Setup backend infra, DB connection and Auth middleware

---
+++ server/middlewares/auth.js
@@ -0,0 +1,30 @@
+import { clerkClient } from "@clerk/express";
+
+// Middleware to check userID and hasPremiumPlan
+
+export const auth = async (req, res, next) => {
+    try {
+        const { userID, has } = await req.auth();
+        const hasPremiumPlan = await has({ plan: 'premium' });
+
+        const user = await clerkClient.users.getUser(userID);
+
+        if (!hasPremiumPlan && user.privateMetadata.free_usage) {
+            req.free_usage = user.privateMetadata.free_usage
+        }
+        else {
+            await clerkClient.users.updateUserMetadata(userID, {
+                privateMetadata: {
+                    free_usage: 0
+                }
+            })
+            req.free_usage = 0;
+        }
+
+        req.plan = hasPremiumPlan ? 'premium' : 'free';
+        next()
+    }
+    catch (error) {
+        res.json({ success: false, message: error.message })
+    }
+}
